f0e837ee806428c6ff36fb06aff89b1d19fe3772
arturodelac <arturodelac1@gmail.com>
2022-01-23 23:36:16 -0600

    Added Capitlization function

--- javascript.js
+++ javascript.js
@@ -2,12 +2,30 @@ function game() {
     let playerCount =0;
     let pcCount=0;
 
+    function capitalize (word) {  
+    word = word.toLowerCase();
+    let firstLetter;
+    firstLetter = word.charAt(0); 
+    firstLetter = firstLetter.toUpperCase();
+    word= word.slice(1);
+    return firstLetter + word;
+    }
+    
+
     do {
-    let userSelection = prompt("Rock, Paper or Scissors?");
-    let ChoicesArray = ["Scissors", "Paper", "Rock"]
+    let userSelection =  prompt("Rock, Paper or Scissors?");
+    console.log(userSelection);
+    userSelection = capitalize(userSelection);
+    console.log(userSelection);
+            
+
+    
+    const ChoicesArray = ["Scissors", "Paper", "Rock"];
+        
          if (ChoicesArray.includes(userSelection)) {
-        console.log("You selected " + userSelection)}
-        else {userSelection = prompt("Rock, Paper or Scissors?")}
+            console.log("You selected " + userSelection)}
+        else {userSelection = prompt("Rock, Paper or Scissors?");
+            userSelection = capitalize(userSelection);}
     const computerChoice = ChoicesArray[Math.floor(Math.random()*ChoicesArray.length)];
         
 
@@ -17,38 +35,38 @@ function game() {
         
         switch(playerChoice) {
             case "Rock": if (randomChoice == "Scissors") {
-                        console.log("You win!");
-                        playerCount++
+                                console.log("You win!");
+                                playerCount++
                         }
                         else if (randomChoice == "Rock") {
-                            console.log("It's a tie!")
+                                console.log("It's a tie!")
                         }
                         else {console.log("You lose! :(");
-                        pcCount++;
+                                pcCount++;
                         }
                         break;
             case "Paper": if (randomChoice == "Rock") {
-                        console.log("You win!");
-                        playerCount++
+                                console.log("You win!");
+                                playerCount++
                         }
                         else if (randomChoice == "Paper") {
-                            console.log("It's a tie!")
-                        }
+                                console.log("It's a tie!")
+                        }   
                         else {console.log("You lose! :(");
-                        pcCount++;
+                                pcCount++;
                         }
                         break;
             
             case "Scissors": if (randomChoice == "Paper") {
-                        console.log("You win!");
-                        playerCount++
-                        }
+                                console.log("You win!");
+                                 playerCount++
+                             }
                         else if (randomChoice == "Scissors") {
-                            console.log("It's a tie!")
-                        }
+                                console.log("It's a tie!")
+                            }
                         else {console.log("You lose! :(");
-                        pcCount++;
-                        }
+                                pcCount++;
+                            }
                         break;
             }
             
